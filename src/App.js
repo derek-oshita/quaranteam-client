453fc02c355d0eaf05704471596053b4511cc0fa
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-08-22 11:49:35 -0700

    Nav connected.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'; 
 import jwt_decode from 'jwt-decode'; 
+import Nav from './components/Nav/Nav'
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        App....
-      </div>
+      <React.Fragment>
+        <Nav />
+      </React.Fragment>
     )
   }
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { NavLink } from 'react-router-dom'; 
+import './Nav.css'; 
+
+function Nav ({ currentUser, logout }) {
+    return (
+        <nav>
+            <p>Nav</p>
+        </nav>
+
+    )
+}; 
+
+export default Nav; 
