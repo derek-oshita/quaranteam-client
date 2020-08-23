4f067994adb5d776de2c0ee1cbc8667ad13ba0d2
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-08-22 17:11:07 -0700

    Login route working.

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import { withRouter } from 'react-router-dom'; 
 import axios from 'axios'; 
+import './Register.css'; 
 
 class Register extends Component {
     state = {
@@ -9,10 +10,49 @@ class Register extends Component {
        password: '',  
     }; 
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value, 
+        }); 
+    }; 
+
+    handleSubmit = (event) => {
+        event.preventDefault(); 
+        axios.post(`${process.env.REACT_APP_API}/auth/register`, this.state)
+            .then((res) => {
+                console.log(res);
+                this.props.history.push('/login');  
+            })
+            .catch((err) => {
+                console.log(err.response.status); 
+                console.log(err.response.data); 
+                console.log(err.response.data.message); 
+            });
+    }
+
     render () {
         return (
-            <div>
-                This is the form....
+            <div className="register-form-container">
+                <h3 className="register">Register</h3>
+                <form onSubmit={this.handleSubmit} className="register-form">
+                    {/* USERNAME */}
+                    <div className="form-item">
+                        <label htmlFor="username">Username: </label>
+                        <input onChange={this.handleChange} type="text" id="username" name="username" value={this.state.username} />
+                    </div>
+                    {/* EMAIL */}
+                    <div className="form-item">
+                        <label htmlFor="username">Email: </label>
+                        <input onChange={this.handleChange} type="text" id="email" name="email" value={this.state.email} />
+                    </div>
+                    {/* PASSWORD */}
+                    <div className="form-item">
+                        <label htmlFor="username">Password: </label>
+                        <input onChange={this.handleChange} type="text" id="password" name="password" value={this.state.password} />
+                    </div>
+                    {/* SUBMIT BUTTON */}
+                    <button type="submit">Register</button>
+                </form>
             </div>
         )
     }
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'; 
 import { Switch, Route } from 'react-router-dom'; 
-
+import Login from '../components/Auth/Login'; 
 import Register from '../components/Auth/Register'; 
 
 export default () => (
-    <Route path='/register' component={Register} />
-)
+    <Switch >
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+    </Switch>
+); 
 
     
