286f7c647275525418c69ece84dba3a698a1ffae
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-09-01 16:26:57 -0700

    Added media query to about page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,13 +10,13 @@ function About() {
             <div className="about-table" id="about">
                 <div className="get">
                     <h4 className="get-h4">Get Real Data</h4>
-                    <p>Using data from Covidtracking.com, you can trust that information regarding the pandemic is both comprehensive and up to date. We also relay helpful metadata around each state, as provided by Civil.Services.</p>
-                    <p className="call-to-action"><Link to="/register">Register</Link></p>
+                    <p className="about-p">Using data from Covidtracking.com, you can trust that information regarding the pandemic is both comprehensive and up to date. We also relay helpful metadata around each state, as provided by Civil.Services.</p>
+                    <p className="call-to-action about-p"><Link to="/register">Register</Link></p>
                 </div>
                 <div className="post">
                     <h4 className="post-h4">Comment On It</h4>
-                    <p>It's rough out there. Every state has handled the pandemic a little differently, so we encourage you to share your experience with other users so we can continue to stay safe and flatten the curve together.</p>
-                    <p className="call-to-action"><Link to="/login">Login</Link></p>
+                    <p className="about-p">It's rough out there. Every state has handled the pandemic a little differently, so we encourage you to share your experience with other users so we can continue to stay safe and flatten the curve together.</p>
+                    <p className="call-to-action about-p"><Link to="/login">Login</Link></p>
                 </div>
 
             </div>
