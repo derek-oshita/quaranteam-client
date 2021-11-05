ab12671d892086b3c93935b7ce7288841bcb5e58
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-11-05 12:57:32 -0700

    IcuTabsCard showing data.

--- src/pages/StateContainer/StateContainerSubcomponents/InfectionCardSubComponents/IcuTabsCard.jsx
+++ src/pages/StateContainer/StateContainerSubcomponents/InfectionCardSubComponents/IcuTabsCard.jsx
@@ -3,22 +3,54 @@ import { Card } from "antd";
 
 const tabList = [
   {
-    key: "tab1",
-    tab: "tab1",
+    key: "Covid ICU Usage",
+    tab: "Covid ICU Usage",
   },
   {
-    key: "tab2",
-    tab: "tab2",
+    key: "Total ICU Usage",
+    tab: "Total ICU Usage",
+  },
+  {
+    key: "Total ICU Capacity",
+    tab: "Total ICU Capacity",
   },
 ];
 
 const IcuTabsCard = (props) => {
-  const { icuBeds: { capacity } = { capacity: 0 } } = props;
-  const { icuBeds: { currentUsageCovid } = { currentUsageCovid: 0 } } = props;
-  const { icuBeds: { currentUsageTotal } = { currentUsageTotal: 0 } } = props;
+  // const [activeTabKey2, setActiveTabKey2] = useState('app');
+  const [activeTabKey, setActiveTabKey] = useState("Covid ICU Usage");
 
-  console.log(currentUsageCovid);
-  return <>IcuTabsCard</>;
+  const onTabChange = (key) => {
+    setActiveTabKey(key);
+  };
+
+  const { icuBeds: { currentUsageCovid } = { currentUsageCovid: 0 } } = props,
+    { icuBeds: { currentUsageTotal } = { currentUsageTotal: 0 } } = props,
+    { icuBeds: { capacity } = { capacity: 0 } } = props;
+
+  const contentList = {
+    "Covid ICU Usage": <p>{currentUsageCovid}</p>,
+    "Total ICU Usage": <p>{currentUsageTotal}</p>,
+    "Total ICU Capacity": <p>{capacity}</p>,
+  };
+
+  return (
+    <>
+      <Card
+        style={{ width: "100%" }}
+        tabList={tabList}
+        activeTabKey={activeTabKey}
+        // tabBarExtraContent={<a href="#">More</a>}
+        onTabChange={(key) => {
+          onTabChange(key);
+        }}
+      >
+        {contentList[activeTabKey]}
+      </Card>
+    </>
+  );
 };
+
+const calculatePercentageOfTotalCapacity = (numberOfBeds) => {};
 
 export default IcuTabsCard;
