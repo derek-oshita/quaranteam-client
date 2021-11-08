706206cceb4667a912dbe90f5a972cd3cb88378a
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-11-08 11:07:24 -0800

    Created a DeathTabsCard.jsx component to represent COVID related deaths.

--- src/pages/StateContainer/StateContainerSubcomponents/InfectionCard.jsx
+++ src/pages/StateContainer/StateContainerSubcomponents/InfectionCard.jsx
@@ -3,6 +3,7 @@ import { Card, Button, Divider, Breadcrumb, PageHeader } from "antd";
 import { UserOutlined, HomeOutlined } from "@ant-design/icons";
 
 import IcuTabsCard from "./InfectionCardSubComponents/IcuTabsCard";
+import DeathTabsCard from "./InfectionCardSubComponents/DeathTabsCard";
 import stateCodeToName from "../../../utils/stateCodeToName";
 
 const InfectionCard = (props) => {
@@ -20,6 +21,7 @@ const InfectionCard = (props) => {
       />
       ,<Divider></Divider>
       <IcuTabsCard icuBeds={icuBeds} />
+      <DeathTabsCard />
     </Card>
   );
 };
---
+++ src/pages/StateContainer/StateContainerSubcomponents/InfectionCardSubComponents/DeathTabsCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeathTabsCard = () => {
+  return <>DeathTabsCard</>;
+};
+
+export default DeathTabsCard;
